c132416dab73c014a1771d8996a664f4d7dc7f3b
Mark Adamcin <adamcin@gmail.com>
2019-08-19 16:40:50 -0700

    Fixed posix script.

--- cli/src/scripts/oakpal.sh
+++ cli/src/scripts/oakpal.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 appHome="$(cd "$(dirname "$(dirname "$0")")" && pwd)"
 oakpalCp=$(ls "${appHome}"/oakpal-cli-*.jar)
-while IFS= read -d $'\0' -r libjar ; do
+for libjar in $(find "${appHome}/lib" -name '*.jar' -type f); do
   oakpalCp="${oakpalCp}:${libjar}"
-done < <(find "${appHome}/lib" -name '*.jar' -type f -print0)
+done
 java ${JAVA_OPTS} -cp "${oakpalCp}" net.adamcin.oakpal.cli.Main "$@"
 
