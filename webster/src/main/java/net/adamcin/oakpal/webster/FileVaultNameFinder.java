5692d873146e13b1e5a5c7fdf77b435485ef54ac
Mark Adamcin <adamcin@gmail.com>
2019-08-26 13:59:10 -0700

    100% coverage for FileVaultNameFinder.

--- webster/src/main/java/net/adamcin/oakpal/webster/FileVaultNameFinder.java
+++ webster/src/main/java/net/adamcin/oakpal/webster/FileVaultNameFinder.java
@@ -16,30 +16,6 @@
 
 package net.adamcin.oakpal.webster;
 
-import static javax.jcr.PropertyType.NAME;
-import static javax.jcr.PropertyType.STRING;
-import static javax.jcr.PropertyType.UNDEFINED;
-import static net.adamcin.oakpal.core.Fun.result1;
-import static net.adamcin.oakpal.core.JsonCnd.BUILTIN_MAPPINGS;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.nio.charset.StandardCharsets;
-import java.util.Collection;
-import java.util.LinkedHashSet;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.Set;
-import java.util.function.Function;
-import java.util.function.Predicate;
-import java.util.stream.Stream;
-import javax.jcr.NamespaceException;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
-
 import net.adamcin.oakpal.core.Fun;
 import net.adamcin.oakpal.core.JsonCnd;
 import net.adamcin.oakpal.core.Result;
@@ -62,10 +38,39 @@ import org.apache.jackrabbit.vault.util.DocViewProperty;
 import org.apache.jackrabbit.vault.util.PlatformNameFormat;
 import org.apache.jackrabbit.vault.util.RejectingEntityDefaultHandler;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
+
+import javax.jcr.NamespaceException;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.StandardCharsets;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
+import java.util.Stack;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+import static javax.jcr.PropertyType.NAME;
+import static javax.jcr.PropertyType.STRING;
+import static javax.jcr.PropertyType.UNDEFINED;
+import static net.adamcin.oakpal.core.Fun.result1;
+import static net.adamcin.oakpal.core.Fun.uncheckVoid1;
+import static net.adamcin.oakpal.core.JsonCnd.BUILTIN_MAPPINGS;
 
 public final class FileVaultNameFinder {
     private static final Logger LOGGER = LoggerFactory.getLogger(FileVaultNameFinder.class);
@@ -166,36 +171,25 @@ public final class FileVaultNameFinder {
             }
 
             if (".xml".equals(ext)) {
-                VaultInputSource is = archive.getInputSource(entry);
-                if (is != null) {
-                    try {
-                        handleDocView(is);
-                    } catch (Exception e) {
-                        // do nothing yet
-                    }
-                }
+                Optional.ofNullable(archive.getInputSource(entry))
+                        .ifPresent(uncheckVoid1((this::handleDocView)));
             } else if (".cnd".equals(ext)) {
-                VaultInputSource is = archive.getInputSource(entry);
-                if (is != null) {
-                    try (InputStream input = is.getByteStream();
-                         Reader reader = new InputStreamReader(input, StandardCharsets.UTF_8)) {
-
-                        CNDReader cndReader = ServiceProviderFactory.getProvider().getCNDReader();
-                        // provide session namespaces
-                        cndReader.read(reader, is.getSystemId(), null);
-                        NamespaceMapping names = cndReader.getNamespaceMapping();
-                        cndReader.getNodeTypes().values().forEach(def -> collectNodeTypeNames(names, def));
-                    }
-                }
+                Optional.ofNullable(archive.getInputSource(entry))
+                        .ifPresent(uncheckVoid1(is -> {
+                            try (InputStream input = is.getByteStream();
+                                 Reader reader = new InputStreamReader(input, StandardCharsets.UTF_8)) {
+                                CNDReader cndReader = ServiceProviderFactory.getProvider().getCNDReader();
+                                // provide session namespaces
+                                cndReader.read(reader, is.getSystemId(), null);
+                                NamespaceMapping names = cndReader.getNamespaceMapping();
+                                cndReader.getNodeTypes().values().forEach(def -> collectNodeTypeNames(names, def));
+                            }
+                        }));
             }
         }
     }
 
-    void handleDocView(final VaultInputSource source) throws ParserConfigurationException, SAXException, IOException {
-        if (source == null) {
-            return;
-        }
-
+    void handleDocView(final @NotNull VaultInputSource source) throws ParserConfigurationException, SAXException, IOException {
         SAXParserFactory factory = SAXParserFactory.newInstance();
         factory.setNamespaceAware(true);
         factory.setFeature("http://xml.org/sax/features/namespace-prefixes", false);
@@ -204,55 +198,77 @@ public final class FileVaultNameFinder {
         parser.parse(source, handler);
     }
 
-
-    class NsStack {
+    static class NsStack {
 
         /**
-         * Next NameSpace element on the stack.
+         * Next NameSpace mapping for this prefix on the stack.
          */
-        public NsStack next = null;
+        final NsStack next;
 
         /**
          * Prefix of this NameSpace element.
          */
-        public String prefix;
-
-        /**
-         * Namespace URI of this NameSpace element.
-         */
-        public String uri;  // if null, then Element namespace is empty.
+        final NamespaceMapping mapping;
 
         /**
          * Construct a namespace for placement on the
          * result tree namespace stack.
          *
-         * @param prefix Prefix of this element
-         * @param uri    URI of  this element
+         * @param mapping the namespace mapping associated with this level of the stack
+         * @param next    the next element in the stack
          */
-        public NsStack(String prefix, String uri) {
-            this.prefix = prefix;
-            this.uri = uri;
+        NsStack(@NotNull NamespaceMapping mapping, @Nullable NsStack next) {
+            this.mapping = mapping;
+            this.next = next;
         }
     }
 
     class Handler extends RejectingEntityDefaultHandler implements NamespaceResolver {
+        boolean expectStartElement = false;
+        boolean expectEndPrefixMapping = false;
+        NamespaceMapping mapping = new NamespaceMapping();
+        NsStack mappingStack = new NsStack(mapping, null);
 
-        final NamespaceMapping mapping = new NamespaceMapping();
         final NamePathResolver npResolver = new DefaultNamePathResolver(this);
         Boolean isDocView = null;
-        NsStack nsStack = null;
+
+        @Override
+        public void startPrefixMapping(final @NotNull String prefix, final @NotNull String uri) throws SAXException {
+            if (!expectStartElement) {
+                mapping = new NamespaceMapping(mapping);
+                mappingStack = new NsStack(mapping, mappingStack);
+                expectStartElement = true;
+                expectEndPrefixMapping = true;
+            }
+            setMapping(prefix, uri);
+        }
+
+        @Override
+        public void endPrefixMapping(final @NotNull String prefix) throws SAXException {
+            if (expectEndPrefixMapping) {
+                if (mappingStack.next == null) {
+                    throw new IllegalStateException("namespace mapping stack is out of sync");
+                }
+                mappingStack = mappingStack.next;
+                mapping = mappingStack.mapping;
+                expectEndPrefixMapping = false;
+            }
+        }
+
+        final void setMapping(final String prefix, final String uri) throws SAXException {
+            try {
+                mapping.setMapping(prefix, uri);
+            } catch (NamespaceException e) {
+                throw new SAXException(e);
+            }
+        }
 
         @Override
         public void startElement(final String uri, final String localName, final String qName, final Attributes attributes) throws SAXException {
-            if (LOGGER.isTraceEnabled()) {
-                LOGGER.trace("[Handler#startElement] uri={} localName={} qName={} attributes={}", uri, localName, qName, attributes);
-            }
+            expectStartElement = false;
+            LOGGER.trace("[Handler#startElement] uri={} localName={} qName={} attributes={}", uri, localName, qName, attributes);
             if (isDocView == null) {
-                if ("jcr:root".equals(qName)) {
-                    isDocView = true;
-                } else {
-                    isDocView = false;
-                }
+                isDocView = "jcr:root".equals(qName);
             }
 
             if (!isDocView) {
@@ -260,33 +276,42 @@ public final class FileVaultNameFinder {
             }
 
             String label = ISO9075.decode(qName);
-            String name = label;
-            int idx = name.lastIndexOf('[');
-            if (idx > 0) {
-                name = name.substring(0, idx);
-            }
+            String name = nameFromLabel(label);
             if (attributes.getLength() > 0) {
-                try {
-                    DocViewNode ni = new DocViewNode(name, label, attributes, npResolver);
-                    if (ni.primary != null) {
-                        Fun.result0(() -> QName.parseQName(mapping, QName.Type.NODETYPE, ni.primary)).get()
-                                .forEach(FileVaultNameFinder.this::addReference);
-                    }
-                    if (ni.mixins != null) {
-                        Stream.of(ni.mixins).map(result1(type -> QName.parseQName(mapping, QName.Type.NODETYPE, type)))
+                DocViewNode ni = createNode(name, label, attributes);
+                if (ni.primary != null) {
+                    Fun.result0(() -> QName.parseQName(mapping, QName.Type.NODETYPE, ni.primary)).get()
+                            .forEach(FileVaultNameFinder.this::addReference);
+                }
+                if (ni.mixins != null) {
+                    Stream.of(ni.mixins).map(result1(type -> QName.parseQName(mapping, QName.Type.NODETYPE, type)))
+                            .flatMap(Result::stream).forEachOrdered(FileVaultNameFinder.this::addReference);
+                }
+                if (ni.props.containsKey("rep:privileges")) {
+                    DocViewProperty prop = ni.props.get("rep:privileges");
+                    if (prop.values != null
+                            && (prop.type == UNDEFINED || prop.type == STRING || prop.type == NAME)) {
+                        Stream.of(prop.values).map(result1(type -> QName.parseQName(mapping, QName.Type.PRIVILEGE, type)))
                                 .flatMap(Result::stream).forEachOrdered(FileVaultNameFinder.this::addReference);
                     }
-                    if (ni.props.containsKey("rep:privileges")) {
-                        DocViewProperty prop = ni.props.get("rep:privileges");
-                        if (prop.values != null
-                                && (prop.type == UNDEFINED || prop.type == STRING || prop.type == NAME)) {
-                            Stream.of(prop.values).map(result1(type -> QName.parseQName(mapping, QName.Type.PRIVILEGE, type)))
-                                    .flatMap(Result::stream).forEachOrdered(FileVaultNameFinder.this::addReference);
-                        }
-                    }
-                } catch (NamespaceException e) {
-                    throw new SAXException(e);
                 }
+            }
+        }
+
+        String nameFromLabel(final @NotNull String label) {
+            int idx = label.lastIndexOf('[');
+            if (idx > 0) {
+                return label.substring(0, idx);
+            } else {
+                return label;
+            }
+        }
+
+        DocViewNode createNode(final String name, final String label, final Attributes attributes) throws SAXException {
+            try {
+                return new DocViewNode(name, label, attributes, npResolver);
+            } catch (NamespaceException e) {
+                throw new SAXException(e);
             }
         }
 
@@ -298,69 +323,6 @@ public final class FileVaultNameFinder {
         @Override
         public String getPrefix(final String uri) throws NamespaceException {
             return mapping.getPrefix(uri);
-        }
-
-        @Override
-        public void startPrefixMapping(final String prefix, final String uri) throws SAXException {
-            LOGGER.trace("[Handler#startPrefixMapping] prefix={} uri={}", prefix, uri);
-            NsStack ns = new NsStack(prefix, uri);
-            // push on stack
-            ns.next = nsStack;
-            nsStack = ns;
-            // check if uri is already registered
-            String oldPrefix = null;
-            try {
-                oldPrefix = mapping.getPrefix(uri);
-            } catch (NamespaceException e) {
-                // assume uri never registered
-                try {
-                    mapping.setMapping(prefix, uri);
-                    oldPrefix = prefix;
-                } catch (NamespaceException e1) {
-                    throw new SAXException(e);
-                }
-            }
-            // update mapping
-            if (!prefix.equals(oldPrefix)) {
-                try {
-                    mapping.setMapping(prefix, uri);
-                } catch (NamespaceException e) {
-                    throw new SAXException(e);
-                }
-            }
-        }
-
-        @Override
-        public void endPrefixMapping(final String prefix) throws SAXException {
-            LOGGER.trace("[Handler#endPrefixMapping] prefix={}", prefix);
-            NsStack ns = nsStack;
-            NsStack prev = null;
-            while (ns != null && !ns.prefix.equals(prefix)) {
-                prev = ns;
-                ns = ns.next;
-            }
-            if (ns == null) {
-                throw new SAXException("Illegal state: prefix " + prefix + " never mapped.");
-            }
-            // remove from stack
-            if (prev == null) {
-                nsStack = ns.next;
-            } else {
-                prev.next = ns.next;
-            }
-            // find old prefix
-            ns = ns.next;
-            while (ns != null && !ns.prefix.equals(prefix)) {
-                ns = ns.next;
-            }
-            // update mapping
-            if (ns != null) {
-                try {
-                    mapping.setMapping(prefix, ns.uri);
-                } catch (NamespaceException e) {
-                    throw new SAXException(e);
-                }
-            }
         }
     }
 }
--- webster/src/test/java/net/adamcin/oakpal/webster/FileVaultNameFinderTest.java
+++ webster/src/test/java/net/adamcin/oakpal/webster/FileVaultNameFinderTest.java
@@ -17,6 +17,7 @@
 package net.adamcin.oakpal.webster;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
@@ -30,6 +31,10 @@ import org.apache.jackrabbit.vault.fs.io.ZipArchive;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.AttributesImpl;
+
+import javax.jcr.NamespaceRegistry;
 
 public class FileVaultNameFinderTest {
     private static final Logger LOGGER = LoggerFactory.getLogger(FileVaultNameFinderTest.class);
@@ -104,5 +109,125 @@ public class FileVaultNameFinderTest {
         LOGGER.info("names: {}", names);
         assertEquals("name search should return one privilege reference",
                 Collections.singleton("crx:replicate"), names);
+    }
+
+    private static FileVaultNameFinder finder() {
+        return new FileVaultNameFinder();
+    }
+
+    private static FileVaultNameFinder.Handler handler() {
+        return finder().new Handler();
+    }
+
+    @Test(expected = SAXException.class)
+    public void testHandler_setMapping_nullPrefix() throws Exception {
+        handler().setMapping(null, "http://foo.com");
+    }
+
+    @Test(expected = SAXException.class)
+    public void testHandler_startPrefixMapping_nullUri() throws Exception {
+        handler().setMapping("nt", null);
+    }
+
+    @Test
+    public void testHandler_startPrefixMapping_remapOld() throws Exception {
+        final FileVaultNameFinder.Handler handler = handler();
+        // push nt
+        handler.startPrefixMapping("nt", NamespaceRegistry.NAMESPACE_NT);
+        handler.expectStartElement = false;
+        assertEquals("expect mapping", "nt",
+                handler.getPrefix(NamespaceRegistry.NAMESPACE_NT));
+        // push jcr (shadow nt)
+        handler.startPrefixMapping("jcr", NamespaceRegistry.NAMESPACE_NT);
+        handler.expectStartElement = false;
+        assertEquals("expect mapping", "jcr",
+                handler.getPrefix(NamespaceRegistry.NAMESPACE_NT));
+        // push jcr
+        handler.startPrefixMapping("jcr", NamespaceRegistry.NAMESPACE_JCR);
+        handler.expectStartElement = false;
+        assertEquals("expect mapping", "jcr",
+                handler.getPrefix(NamespaceRegistry.NAMESPACE_JCR));
+        assertEquals("expect jcr prefix for nt uri", "jcr",
+                handler.getPrefix(NamespaceRegistry.NAMESPACE_NT));
+        assertEquals("expect nt uri for nt prefix", NamespaceRegistry.NAMESPACE_NT,
+                handler.getURI("nt"));
+        // push nt1 (shadow nt)
+        handler.startPrefixMapping("nt1", NamespaceRegistry.NAMESPACE_NT);
+        handler.expectStartElement = false;
+        assertEquals("expect mapping", "nt1",
+                handler.getPrefix(NamespaceRegistry.NAMESPACE_NT));
+
+        // push nt2 (shadow nt1)
+        handler.startPrefixMapping("nt2", NamespaceRegistry.NAMESPACE_NT);
+        handler.expectStartElement = false;
+        assertEquals("expect mapping", "nt2",
+                handler.getPrefix(NamespaceRegistry.NAMESPACE_NT));
+        assertEquals("expect jcr mapping", "jcr",
+                handler.getPrefix(NamespaceRegistry.NAMESPACE_JCR));
+
+        // pop nt2 (reveal nt1) -> active: nt1, jcr
+        handler.endPrefixMapping("nt2");
+        handler.expectEndPrefixMapping = true;
+        assertEquals("expect mapping after end nt2", "nt1",
+                handler.getPrefix(NamespaceRegistry.NAMESPACE_NT));
+        assertEquals("expect jcr mapping", "jcr",
+                handler.getPrefix(NamespaceRegistry.NAMESPACE_JCR));
+
+        // pop nt1 (reveal
+        handler.endPrefixMapping("nt1");
+        handler.expectEndPrefixMapping = true;
+        assertEquals("expect nt uri for nt prefix", NamespaceRegistry.NAMESPACE_NT,
+                handler.getURI("nt"));
+        assertEquals("expect jcr prefix for nt uri", "jcr",
+                handler.getPrefix(NamespaceRegistry.NAMESPACE_NT));
+        assertEquals("expect jcr mapping", "jcr",
+                handler.getPrefix(NamespaceRegistry.NAMESPACE_JCR));
+        // pop jcr
+        handler.endPrefixMapping("jcr");
+        handler.expectEndPrefixMapping = true;
+        assertEquals("expect nt uri for nt prefix", NamespaceRegistry.NAMESPACE_NT,
+                handler.getURI("nt"));
+        assertEquals("expect nt uri for jcr prefix", NamespaceRegistry.NAMESPACE_NT,
+                handler.getURI("jcr"));
+        assertEquals("expect jcr prefix for nt uri", "jcr",
+                handler.getPrefix(NamespaceRegistry.NAMESPACE_NT));
+
+        handler.endPrefixMapping("jcr");
+        handler.expectEndPrefixMapping = true;
+        assertFalse("expect no registered jcr prefix", handler.mapping.hasPrefix("jcr"));
+        assertEquals("expect nt uri for nt prefix", NamespaceRegistry.NAMESPACE_NT,
+                handler.getURI("nt"));
+        assertEquals("expect nt prefix for nt uri", "nt",
+                handler.getPrefix(NamespaceRegistry.NAMESPACE_NT));
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testHandler_endPrefixMapping_illegalState() throws Exception {
+        FileVaultNameFinder.Handler handler = handler();
+        handler.expectEndPrefixMapping = true;
+        handler.endPrefixMapping("foo");
+    }
+
+    @Test(expected = SAXException.class)
+    public void testHandler_createNode() throws Exception {
+        final AttributesImpl attributes = new AttributesImpl();
+        attributes.addAttribute(NamespaceRegistry.NAMESPACE_JCR, "primaryType", "jcr:primaryType", "CDATA", "nt:folder");
+        handler().createNode("name", "label", attributes);
+    }
+
+    @Test
+    public void testHandler_startElement_notDocViewReturnsSuccessfully() throws Exception {
+        FileVaultNameFinder.Handler handler = handler();
+        handler.isDocView = false;
+        handler.startElement(null, null, null, null);
+    }
+
+    @Test
+    public void testHandler_nameFromLabel() {
+        assertEquals("foo name is", "foo", handler().nameFromLabel("foo"));
+        assertEquals("'' name is", "", handler().nameFromLabel(""));
+        assertEquals("foo[] name is", "foo", handler().nameFromLabel("foo[]"));
+        assertEquals("foo[0] name is", "foo", handler().nameFromLabel("foo[0]"));
+        assertEquals("foo[anystringofchars] name is", "foo", handler().nameFromLabel("foo[anystringofchars]"));
     }
 }
