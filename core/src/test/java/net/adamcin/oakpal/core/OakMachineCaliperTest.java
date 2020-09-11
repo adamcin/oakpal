abbcf9d01debaefc67746ef4ec9a7a2a11904907
Mark Adamcin <adamcin@gmail.com>
2020-09-11 08:36:08 -0700

    fixed caliper test assumptions

--- core/src/test/java/net/adamcin/oakpal/core/OakMachineCaliperTest.java
+++ core/src/test/java/net/adamcin/oakpal/core/OakMachineCaliperTest.java
@@ -31,38 +31,72 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static net.adamcin.oakpal.api.JavaxJson.obj;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class OakMachineCaliperTest {
 
     private File grandTourPackage = TestPackageUtil.getCaliperPackage();
 
-    private List<String> installOrderFull = Arrays.asList(
-            "oakpal-caliper.all",
-            "oakpal-caliper.ui.apps",
-            "oakpal-caliper.ui.apps.author",
-            "oakpal-caliper.ui.apps.publish",
-            "oakpal-caliper.ui.content",
-            "oakpal-caliper.ui.content.subc1",
-            // not actually installed: "oakpal-caliper.ui.content.suba2",
-            "oakpal-caliper.ui.content.subb3"
-    );
+    private Map<String, List<String>> dependsOn = new HashMap<>();
 
-    private List<String> installOrderNoRunModesNoSubs = Arrays.asList(
-            "oakpal-caliper.all",
-            "oakpal-caliper.ui.apps",
-            "oakpal-caliper.ui.content"
-    );
+    private static final String NAME_ALL = "oakpal-caliper.all";
+    private static final String NAME_APPS = "oakpal-caliper.ui.apps";
+    private static final String NAME_APPS_AUTHOR = "oakpal-caliper.ui.apps.author";
+    private static final String NAME_APPS_PUBLISH = "oakpal-caliper.ui.apps.publish";
+    private static final String NAME_CONTENT = "oakpal-caliper.ui.content";
+    private static final String NAME_CONTENT_SUBC1 = "oakpal-caliper.ui.content.subc1";
+    private static final String NAME_CONTENT_SUBA2 = "oakpal-caliper.ui.content.suba2";
+    private static final String NAME_CONTENT_SUBB3 = "oakpal-caliper.ui.content.subb3";
+
+    private Set<String> allNames = new HashSet<>(Arrays.asList(
+            NAME_ALL,
+            NAME_APPS,
+            NAME_APPS_AUTHOR,
+            NAME_APPS_PUBLISH,
+            NAME_CONTENT,
+            NAME_CONTENT_SUBC1,
+            NAME_CONTENT_SUBA2,
+            NAME_CONTENT_SUBB3
+    ));
+
+    private Set<String> installOrderFull = new HashSet<>(Arrays.asList(
+            NAME_ALL,
+            NAME_APPS,
+            NAME_APPS_AUTHOR,
+            NAME_APPS_PUBLISH,
+            NAME_CONTENT,
+            NAME_CONTENT_SUBC1,
+            // not actually installed: "oakpal-caliper.ui.content.suba2",
+            NAME_CONTENT_SUBB3
+    ));
+
+    private Set<String> installOrderNoRunModesNoSubs = new HashSet<>(Arrays.asList(
+            NAME_ALL,
+            NAME_APPS,
+            NAME_CONTENT
+    ));
 
     @Before
     public void setUp() throws Exception {
+        // populate with direct dependencies as notated in caliper poms
+        dependsOn.put(NAME_APPS, Collections.singletonList(NAME_ALL));
+        dependsOn.put(NAME_APPS_AUTHOR, Collections.singletonList(NAME_APPS));
+        dependsOn.put(NAME_APPS_PUBLISH, Collections.singletonList(NAME_APPS));
+        dependsOn.put(NAME_CONTENT, Arrays.asList(NAME_ALL, NAME_APPS));
+        dependsOn.put(NAME_CONTENT_SUBC1, Collections.singletonList(NAME_CONTENT));
+        dependsOn.put(NAME_CONTENT_SUBB3, Arrays.asList(NAME_CONTENT, NAME_CONTENT_SUBC1));
+
         assertTrue("expect grand tour package has been copied to " + grandTourPackage.getAbsolutePath(),
                 grandTourPackage.exists());
     }
@@ -106,7 +140,8 @@ public class OakMachineCaliperTest {
                 .build()
                 .scanPackage(grandTourPackage);
 
-        assertEquals("expect identified packages in order", installOrderNoRunModesNoSubs,
+        assertAllDependenciesInList("testScanNoRunModes_captureIdentifiedPackages",
+                installOrderNoRunModesNoSubs,
                 identifiedPackageIds.stream().map(PackageId::getName).collect(Collectors.toList()));
     }
 
@@ -143,7 +178,40 @@ public class OakMachineCaliperTest {
                 .build()
                 .scanPackage(grandTourPackage);
 
-        assertEquals("expect identified packages in set", installOrderFull,
+        assertAllDependenciesInList("testScanAllRunModesWithSubs_captureIdentifiedPackages",
+                installOrderFull,
                 identifiedPackageIds.stream().map(PackageId::getName).collect(Collectors.toList()));
+    }
+
+    public Collection<String> getDependencies(final String packageName) {
+        return dependsOn.getOrDefault(packageName, Collections.emptyList());
+    }
+
+    public void assertAllDependenciesInList(final String message,
+                                            final Set<String> expected,
+                                            final List<String> actual) {
+        for (String expectedName : expected) {
+            Collection<String> dependencies = getDependencies(expectedName).stream()
+                    .filter(expected::contains)
+                    .collect(Collectors.toSet());
+            for (String dependency : dependencies) {
+                assertStringBeforeAfter(message, actual, dependency, expectedName);
+            }
+        }
+        final Set<String> expectMissing = new HashSet<>(allNames);
+        expectMissing.removeAll(expected);
+        for (String unexpectedName : expectMissing) {
+            assertFalse(String.format(message + ": expect %s not contains %s", actual, unexpectedName),
+                    actual.contains(unexpectedName));
+        }
+    }
+
+    public void assertStringBeforeAfter(final String message, final List<String> elements, final String before, final String after) {
+        final int indexBefore = elements.indexOf(before);
+        assertTrue(String.format(message + ": expect %s contains before %s", elements, before), indexBefore >= 0);
+        final int indexAfter = elements.indexOf(after);
+        assertTrue(String.format(message + ": expect %s contains after %s", elements, after), indexAfter >= 0);
+        assertTrue(String.format(message + ": expect in %s, before %s prior to after %s", elements, before, after),
+                indexBefore < indexAfter);
     }
 }
